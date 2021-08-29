747b8bc8dd59e6b75a5d94eb206769a405eac6e5
Rakshith <rakshith.2905@gmail.com>
2021-08-29 10:31:07 -0700

    removed weights on edge computation

--- metadag.py
+++ metadag.py
@@ -58,9 +58,12 @@ class MetaGraph(object):
                 if idx_i == idx_j:
                     dist = tf.squeeze(tf.zeros([1]))
                 else:
-                    dist = tf.squeeze(tf.sigmoid(tf.layers.dense(
-                        tf.abs(self.node_cluster_center[idx_i] - self.node_cluster_center[idx_j]), units=1,
-                        name='meta_dist_' + name + '_node_' + str(idx_i)+ '_to_' + str(idx_j))))
+                    # dist = tf.squeeze(tf.sigmoid(tf.layers.dense(
+                    #     tf.abs(self.node_cluster_center[idx_i] - self.node_cluster_center[idx_j]), units=1,
+                    #     name='meta_dist_' + name + '_node_' + str(idx_i)+ '_to_' + str(idx_j))))
+                    dist = tf.squeeze(tf.sigmoid(
+                        tf.math.reduce_euclidean_norm(self.node_cluster_center[idx_i] - self.node_cluster_center[idx_j]),
+                        name='meta_dist'))
                 tmp_dist.append(dist)
             meta_graph.append(tf.stack(tmp_dist))
         self.meta_graph_edges = tf.stack(meta_graph, name='meta_graph_edges_' + name)
@@ -99,8 +102,10 @@ class MetaGraph(object):
                 if idx_i == idx_j:
                     dist = tf.squeeze(tf.zeros([1]))
                 else:
-                    dist = tf.squeeze(tf.sigmoid(tf.layers.dense(
-                        tf.abs(tf.expand_dims(inputs[idx_i] - inputs[idx_j], axis=0)), units=1, name='proto_dist')))
+                    # dist = tf.squeeze(tf.sigmoid(tf.layers.dense(
+                    #     tf.abs(tf.expand_dims(inputs[idx_i] - inputs[idx_j], axis=0)), units=1, name='proto_dist')))
+                    dist = tf.squeeze(tf.sigmoid(
+                        tf.math.reduce_euclidean_norm(inputs[idx_i] - inputs[idx_j]),name='proto_dist'))
                 tmp_dist.append(dist)
             proto_graph.append(tf.stack(tmp_dist))
         proto_graph = tf.stack(proto_graph)
